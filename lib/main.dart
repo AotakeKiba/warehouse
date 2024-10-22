82e0d2dd00a394d2b473cce5e2d555efd9f0af6b
AotakeKiba <evanrahardja010398@gmail.com>
2024-10-22 10:34:50 +0700

    working_counting_words

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,16 @@ class Warehouse extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Login(title: 'Login'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Warehouse',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Login(title: 'Login'),
+      )
     );
   }
 }
@@ -121,7 +124,6 @@ class _LoginState extends State<Login> {
   }
 }
 
-// for variables and functions
 class MyAppState extends ChangeNotifier {
   var current = WordPair(' ', ' ');
   
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:learning_flutter/main.dart';
 
-import 'package:warehouse/main.dart';
+// import 'package:warehouse/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
