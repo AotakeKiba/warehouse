4135e06118b5b4f250b00e188b582a698f54a369
AotakeKiba <evanrahardja010398@gmail.com>
2024-10-22 10:01:32 +0700

    add_sidebar_and_wordpair

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 final _formKey = GlobalKey<FormState>();
 TextEditingController emailController = TextEditingController();
 TextEditingController passwordController = TextEditingController();
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Warehouse());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Warehouse extends StatelessWidget {
+  const Warehouse({super.key});
 
   // This widget is the root of your application.
   @override
@@ -20,21 +22,21 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Warehouse(title: 'Login'),
+      home: const Login(title: 'Login'),
     );
   }
 }
 
-class Warehouse extends StatefulWidget {
-  const Warehouse({super.key, required this.title});
+class Login extends StatefulWidget {
+  const Login({super.key, required this.title});
 
   final String title;
 
   @override
-  State<Warehouse> createState() => _WarehouseState();
+  State<Login> createState() => _LoginState();
 }
 
-class _WarehouseState extends State<Warehouse> {
+class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
@@ -83,21 +85,34 @@ class _WarehouseState extends State<Warehouse> {
             Padding(
               padding:
                 const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Navigate the user to the Home page
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Please fill input')),
-                        );
-                      }
-                    },
-                  child: const Text('Submit'),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          if (emailController.text == "test" &&
+                                        passwordController.text == "test") {
+                              Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MyHomePage(),
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Invalid Credentials')),
+                              );
+                          }
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Please fill input')),
+                          );
+                        }
+                      },
+                    child: const Text('Submit'),
                   ),
                 ),
-              ),
+            ),
             ],
           ),
         ),
@@ -106,55 +121,214 @@ class _WarehouseState extends State<Warehouse> {
   }
 }
 
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding:
-                const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        if (emailController.text == "arun@gogosoon.com" &&
-                                      passwordController.text == "qazxswedcvfr") {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => HomePage(
-                                email: emailController.text,
-                              )),
-                          );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Invalid Credentials')),
-                            );
-                        }
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Please fill input')),
-                        );
-                      }
-                    },
-                  child: const Text('Submit'),
-                  ),
-                ),
-            ),
-            Text(email),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Go back!"),
-              ),
-            ),
-          ],
-        ));
+// for variables and functions
+class MyAppState extends ChangeNotifier {
+  var current = WordPair(' ', ' ');
+  
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
   }
+
+  var favorites = <WordPair>[]; // list
+
+  void toggleFavourites(){
+    if (favorites.contains(current)){
+      favorites.remove(current);
+    }else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
+  var current2 = WordPair('first', 'second');
+
+  int counter = 0;
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      case 2:
+        page = ReportPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600, // -> true to expand the navigation bar
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.report),
+                      label: Text('Report'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex, //-> to select default menu when start
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavourites();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                  appState.counter++;
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+class ReportPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  var appState = context.watch<MyAppState>();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('you have clicked ''${appState.counter} times'),
+          ),
+      ],
+    ),
+    );throw UnimplementedError();
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    //for color
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary, backgroundColor: theme.colorScheme.secondary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      //color: theme.colorScheme.fromSeed(seedColor: Colors.green),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Text(pair.asLowerCase, style: style, semanticsLabel: "${pair.first} ${pair.second}",),
+      ),
+    );
+  }
+}
